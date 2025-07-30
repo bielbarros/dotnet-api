924fe342f30579cfad6b999d4fd3f0cc7a724bf7
biel.barros <gabrielbarrosg11@gmail.com>
2025-07-30 17:31:40 -0300

    feat: add Pizza model with CRUD operations

---
+++ Models/Pizza.cs
@@ -0,0 +1,8 @@
+namespace Exercício_MS_C__WEB_API.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+}
